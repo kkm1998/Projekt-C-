576fbbc7dc2a8626ae4e4b71bea5c9444508b66a
kkm1998 <michalkowalski132@gmail.com>
2019-05-24 14:08:20 +0200

    copied from friend

---
+++ SongSwears/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongSwears
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var songAnalysis = new SongAnalysis("Kazik", "12 groszy");
+        }
+    }
+}
